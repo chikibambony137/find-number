bfa2b2f85283874bc859ce9e2e04ffaf0b3f106f
Anton <137686641+chikibambony137@users.noreply.github.com>
2025-06-05 18:17:22 +0300

    make base game mechanic

--- main.js
+++ main.js
@@ -9,8 +9,6 @@ function countdown() {
       clearInterval(countdown);
       countdownElement.parentElement.style.display = "none";
 
-      gameDiv.style.backgroundImage =
-        "url('../assets/images/game-background-light.png')";
       gameDiv.style.backgroundSize = "800px";
       gameDiv.style.backgroundPosition = "-90px 0px";
       overlayDiv.style.display = "none";
@@ -19,4 +17,85 @@ function countdown() {
       timeLeft--;
     }
   }, 1000);
+}
+
+function generateNumber() {
+
+  let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  let randomNumber = Math.floor(Math.random() * 10);
+  document.getElementById("number").innerHTML = numbers[randomNumber];
+  document.getElementById("variant").innerHTML = numbers[randomNumber];
+  numbers.splice(randomNumber, 1);
+
+  console.log(numbers);
+
+  const variants = document.querySelectorAll(".variant");
+  let index = 0;
+  numbers.forEach((number) => {
+    variants[index].innerHTML = number;
+    numbers.splice(index, 1);
+    console.log(numbers);
+    index++;
+  });
+}
+
+function randomizeVariants() {
+  // Функция для перемешивания массива
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  // Получаем все элементы с классом 'variant' и 'variant' id
+  const variantsContainer = document.querySelector(".variants");
+  const variants = Array.from(variantsContainer.children);
+
+  // Перемешиваем элементы
+  const shuffledVariants = shuffleArray(variants);
+
+  // Очищаем контейнер и добавляем перемешанные элементы обратно
+  variantsContainer.innerHTML = "";
+  shuffledVariants.forEach((variant) => {
+    variantsContainer.appendChild(variant);
+  });
+}
+
+function game(){
+  let correctAnswers = 0;
+  let wrongAnswers = 0;
+
+  const variants = document.querySelectorAll('.variant, #variant');
+
+  variants.forEach(variant => {
+      variant.addEventListener('click', () => {
+          if (variant.id === 'variant') {
+              correctAnswers++;
+              console.log(correctAnswers);
+              document.getElementById('correct').innerHTML = correctAnswers;
+              goNext();
+
+          } else {
+              wrongAnswers++;
+              console.log(wrongAnswers);
+              document.getElementById('wrong').innerHTML = wrongAnswers;
+              goNext();
+          }
+      });
+  });
+}
+
+function startGame() {
+  countdown();
+  generateNumber();
+  randomizeVariants();
+  game();
+}
+
+function goNext() {
+  generateNumber();
+  randomizeVariants();
 }
