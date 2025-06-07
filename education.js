94b675b58e6c8079c3e70a291ec2e0fca413e216
Anton <137686641+chikibambony137@users.noreply.github.com>
2025-06-07 14:51:24 +0300

    add education

---
+++ education.js
@@ -0,0 +1,6 @@
+function education() {
+    const contentDiv = document.querySelector(".content");
+    contentDiv.addEventListener('click', function() {
+        window.location.href = "./game.html";
+    })
+}
